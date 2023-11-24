e8fea4dc16ccb3d9e27d004969b237bd2e75b97b
Joseph Scott <joseph@josephscott.org>
2023-11-24 13:39:11 -0700

    Tests for GET

---
+++ tests/get-tests.php
@@ -0,0 +1,11 @@
+<?php
+declare( strict_types = 1 );
+
+test( 'get', function () {
+	$http = new Amulet\HTTP_Request();
+	$response = $http->get( url: 'http://127.0.0.1:7878/' );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+} );
