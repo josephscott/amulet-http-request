f94420aa281a6ddf226b60cfce6d5f8440b1f1fa
Joseph Scott <joseph@josephscott.org>
2023-11-24 13:38:30 -0700

    Parse headers via curl callback

--- src/http-request.php
+++ src/http-request.php
@@ -19,6 +19,8 @@ class HTTP_Request {
 	) : array {
 		$out = [
 			'error' => false,
+			'response_code' => 0,
+			'http_version' => 0,
 			'headers' => [],
 			'body' => '',
 			'timing' => [],
@@ -36,7 +38,33 @@ class HTTP_Request {
 			CURLOPT_FOLLOWLOCATION => false,
 			CURLOPT_PROTOCOLS => CURLPROTO_HTTP | CURLPROTO_HTTPS,
 			CURLOPT_HTTPHEADER => $headers,
+			CURLOPT_HEADERFUNCTION => function ( $curl, $header ) use ( &$out ) {
+				$length = strlen( $header );
+				$parts = explode( ':', $header, 2 );
+
+				if ( count( $parts ) < 2 ) {
+					if ( preg_match( '/^HTTP\/([0-9\.]+)\s+([0-9]+)/', $header, $matches ) ) {
+						$out['http_version'] = (int) $matches[1];
+						$out['response_code'] = (int) $matches[2];
+						return $length;
+					} else {
+						// Invalid header
+						return $length;
+					}
+				}
+
+				$key = strtolower( trim( $parts[0] ) );
+				$value = trim( $parts[1] );
+				if ( is_numeric( $value ) ) {
+					$value = (int) $value;
+				}
+
+				// May need to reconsider this for duplicate headers
+				$out['headers'][$key] = $value;
+				return $length;
+			},
 		] );
+
 		$response = curl_exec( $curl );
 		if ( $response === false ) {
 			$out['error'] = true;
