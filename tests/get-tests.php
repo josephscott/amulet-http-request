2e6c07eaef26c721137fbfc975056128a3fd5276
Joseph Scott <joseph@josephscott.org>
2023-11-25 09:28:44 -0700

    Check the method returned

--- tests/get-tests.php
+++ tests/get-tests.php
@@ -5,9 +5,13 @@ test( 'get', function () {
 	$http = new \Amulet\HTTP_Request();
 	$response = $http->get( url: 'http://127.0.0.1:7878/' );
 
+	$data = json_decode( $response['body'], associative: true );
+
 	expect( $response['error'] )->toBe( false );
 	expect( $response['response_code'] )->toBe( 200 );
 	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+
+	expect( $data['method'] )->toBe( 'get' );
 } );
 
 test( 'get: timeout', function () {
