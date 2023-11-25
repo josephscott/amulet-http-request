82eebd708fa6e705aed0625676c4fa27b6eeedd0
Joseph Scott <joseph@josephscott.org>
2023-11-24 18:03:55 -0700

    New test for GET query vars

--- tests/get-tests.php
+++ tests/get-tests.php
@@ -18,3 +18,15 @@ test( 'get: timeout', function () {
 	expect( $response['error'] )->toBe( true );
 	expect( $response['response_code'] )->toBe( 0 );
 } );
+
+test( 'get: query vars', function () {
+	$http = new \Amulet\HTTP_Request();
+	$response = $http->get( url: 'http://127.0.0.1:7878/?hello=world' );
+
+	$data = json_decode( $response['body'], associative: true );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+	expect( $data['get']['hello'] )->toBe( 'world' );
+} );
