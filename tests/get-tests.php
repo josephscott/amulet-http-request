0af124857c8cfee36dcd71386ac74e4d4eea8644
Joseph Scott <joseph@josephscott.org>
2023-11-24 20:04:59 -0700

    Add GET test to confirm response timing details

--- tests/get-tests.php
+++ tests/get-tests.php
@@ -30,3 +30,19 @@ test( 'get: query vars', function () {
 	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
 	expect( $data['get']['hello'] )->toBe( 'world' );
 } );
+
+test( 'get: response timing', function () {
+	$http = new \Amulet\HTTP_Request();
+	$response = $http->get( url: 'http://127.0.0.1:7878/?hello=world' );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+
+	expect( $response['timing']['dns'] )->toBeGreaterThan( 0 );
+	expect( $response['timing']['tcp'] )->toBeGreaterThan( 0 );
+	expect( $response['timing']['tls'] )->toBe( 0 );
+	expect( $response['timing']['redirect'] )->toBe( 0 );
+	expect( $response['timing']['http'] )->toBeGreaterThan( 0 );
+	expect( $response['timing']['total'] )->toBeGreaterThan( 0 );
+} );
