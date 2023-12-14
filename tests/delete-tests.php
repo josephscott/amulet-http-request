b1652810dd20178efcb8a9d487d571906b51b04b
Joseph Scott <joseph@josephscott.org>
2023-12-13 21:31:42 -0700

    Expand DELETE tests to include php approach

--- tests/delete-tests.php
+++ tests/delete-tests.php
@@ -2,7 +2,19 @@
 declare( strict_types = 1 );
 
 test( 'delete', function () {
+	// curl
 	$http = new \Amulet\HTTP\Request();
+	$response = $http->delete( url: 'http://127.0.0.1:7878/?method=delete' );
+
+	$data = json_decode( $response['body'], associative: true );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+
+	// php
+	$http = new \Amulet\HTTP\Request();
+	$http->default_options['using'] = 'php';
 	$response = $http->delete( url: 'http://127.0.0.1:7878/?method=delete' );
 
 	$data = json_decode( $response['body'], associative: true );
