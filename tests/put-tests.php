161830bae04e1e9c0c778a041abce912b8f3d5f0
Joseph Scott <joseph@josephscott.org>
2023-12-13 22:34:59 -0700

    Add php approach for PUT tests

--- tests/put-tests.php
+++ tests/put-tests.php
@@ -2,7 +2,25 @@
 declare( strict_types = 1 );
 
 test( 'post: data', function () {
+	// curl
 	$http = new \Amulet\HTTP\Request();
+	$response = $http->put(
+		url: 'http://127.0.0.1:7878/?method=put',
+		data: [
+			'this' => 'here',
+			'hello' => 'world',
+		]
+	);
+
+	$data = json_decode( $response['body'], associative: true );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+
+	// php
+	$http = new \Amulet\HTTP\Request();
+	$http->default_options['using'] = 'php';
 	$response = $http->put(
 		url: 'http://127.0.0.1:7878/?method=put',
 		data: [
