07398293a27ffe440fa08b8dd62f31359687c60f
Joseph Scott <joseph@josephscott.org>
2023-12-13 22:42:13 -0700

    Add POST tests with the php approach

--- tests/post-tests.php
+++ tests/post-tests.php
@@ -2,7 +2,26 @@
 declare( strict_types = 1 );
 
 test( 'post: data', function () {
+	// curl
 	$http = new \Amulet\HTTP\Request();
+	$response = $http->post(
+		url: 'http://127.0.0.1:7878/?method=post',
+		data: [
+			'this' => 'here',
+			'hello' => 'world',
+		]
+	);
+
+	$data = json_decode( $response['body'], associative: true );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+	expect( $data['post']['hello'] )->toBe( 'world' );
+
+	// php
+	$http = new \Amulet\HTTP\Request();
+	$http->default_options['using'] = 'php';
 	$response = $http->post(
 		url: 'http://127.0.0.1:7878/?method=post',
 		data: [
