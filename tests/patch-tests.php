d069cd15c59fefbdc22788293138d2eddefa4d30
Joseph Scott <joseph@josephscott.org>
2023-12-14 20:51:01 -0700

    Add PATCH tests using the php approach

--- tests/patch-tests.php
+++ tests/patch-tests.php
@@ -1,8 +1,26 @@
 <?php
 declare( strict_types = 1 );
 
-test( 'patch: data', function () {
+test( 'patch-curl: data', function () {
 	$http = new \Amulet\HTTP\Request();
+	$response = $http->patch(
+		url: 'http://127.0.0.1:7878/?method=patch',
+		data: [
+			'this' => 'here',
+			'hello' => 'world',
+		]
+	);
+
+	$data = json_decode( $response['body'], associative: true );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+} );
+
+test( 'patch-php: data', function () {
+	$http = new \Amulet\HTTP\Request();
+	$http->default_options['using'] = 'php';
 	$response = $http->patch(
 		url: 'http://127.0.0.1:7878/?method=patch',
 		data: [
