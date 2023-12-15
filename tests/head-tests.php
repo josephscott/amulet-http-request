75a6e23e14db547f24a5a4708da2e3740873f239
Joseph Scott <joseph@josephscott.org>
2023-12-14 21:26:06 -0700

    Add HEAD tests using php

--- tests/head-tests.php
+++ tests/head-tests.php
@@ -1,8 +1,18 @@
 <?php
 declare( strict_types = 1 );
 
-test( 'head', function () {
+test( 'head-curl', function () {
 	$http = new \Amulet\HTTP\Request();
+	$response = $http->get( url: 'http://127.0.0.1:7878/' );
+
+	expect( $response['error'] )->toBe( false );
+	expect( $response['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
+} );
+
+test( 'head-php', function () {
+	$http = new \Amulet\HTTP\Request();
+	$http->default_options['using'] = 'php';
 	$response = $http->get( url: 'http://127.0.0.1:7878/' );
 
 	expect( $response['error'] )->toBe( false );
