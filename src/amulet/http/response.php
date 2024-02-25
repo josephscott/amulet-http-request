ded200636c886425179990011b0053b448e1581f
Joseph Scott <joseph@josephscott.org>
2024-02-24 22:13:29 -0700

    Response: rename response_code to code, add using field

--- src/amulet/http/response.php
+++ src/amulet/http/response.php
@@ -4,8 +4,9 @@ declare( strict_types = 1 );
 namespace Amulet\HTTP;
 
 class Response {
+	public string $using = '';
 	public bool $error = false;
-	public int $response_code = 0;
+	public int $code = 0;
 	public float $http_version = 0.0;
 	public array $headers = [];
 	public array $timing = [];
